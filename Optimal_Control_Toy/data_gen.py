e3ef0b45b95ad47c0a9774cf787b0804b55367d9
s-hallsworth <s.d.hallsworth@student.tudelft.nl>
2024-04-05 15:55:31 +0200

    update optimal control transformer

--- Optimal_Control_Toy/data_gen.py
+++ Optimal_Control_Toy/data_gen.py
@@ -3,20 +3,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 N = 3 # number of functions
-T = 10 
+T = 10
 cols = ['time', 'u', 'x']
 x = np.zeros((N,T))
 u = np.zeros((N,T))
 time = np.linspace(0,1,num=T)
 
+x_prime_diff = np.zeros((N,T))
+x_prime_u = np.zeros((N,T))
+
 def func_1(t):
     u = 1/(2*(2-t))
-    x = t + 1/(4*(2-t))
+    x = t + 1/(8-(4*t))
     return u,x
     
 def func_2(t):
-    u = 1/t
-    x = t - (1/t)
+    # u = 1/t
+    # x = t - (1/t)
+    u = t
+    x = t + (t**3)/3
     return u,x
     
 def func_3(t):
@@ -35,9 +40,17 @@ for t in range(T):
     u[2,t], x[2,t] = func_3(time[t])
     #u[3,t], x[3,t] = func_4(time[t])
     
+x[:,0] = 1 #set intial condition   
+for t in range(T):    
+    x_prime_diff[:,t-1] = (x[:,t]-x[:,t-1])/(time[t]-time[t-1]) #dx/dt
+x_prime_u = 1 + u**2
+
+print(u.shape)
+print(x_prime_diff.shape)
+print(x_prime_u.shape )
 
 # Plotting
-plt.figure(figsize=(12, 8))
+plt.figure(1,figsize=(12, 8))
 markers = ['o-', 's-', '^-', 'd-']  # Different markers for each function
 for i in range(N):
     plt.plot(time, x[i, :], markers[i], label=f'x (Position) from Func {i+1}')
@@ -50,9 +63,20 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-df = pd.DataFrame(time)
+plt.figure(2,figsize=(12, 8))
+markers = ['o-', 's-', '^-', 'd-']  # Different markers for each function
+for i in range(N):
+    plt.plot(time, x_prime_diff[i, :] - x_prime_u[i, :], markers[i], label=f'dx/dt - 1+u^2 from Func {i+1}')
+    #plt.plot(time, x_prime_u[i, :], markers[i], linestyle='dashed', label=f'1+u^2 from Func {i+1}')
 
+plt.title('Calculating dx/dt via x and u '+str(N)+' Functions')
+plt.xlabel('Time')
+plt.ylabel('Magnitude')
+plt.legend()
+plt.grid(True)
+plt.show()
 
+#Save data
 data = {
     'time': np.tile(time, N),  # Repeat 'time' N times
     'u': u.flatten(),  # Flatten 'u' to match 'time'
