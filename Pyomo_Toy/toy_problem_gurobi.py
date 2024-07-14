a70cdcb45d0cc81b2020f7fe257b972304b78857
s-hallsworth <s.d.hallsworth@student.tudelft.nl>
2024-07-14 15:44:18 -0400

    add std constraint

--- Pyomo_Toy/toy_problem_gurobi.py
+++ Pyomo_Toy/toy_problem_gurobi.py
@@ -28,7 +28,7 @@ transformer = TNN.Transformer(model, config_file)
         
 # Define tranformer layers
 transformer.embed_input(model, "input_param","input_embed", "variables")
-transformer.add_layer_norm(model, "input_embed", "layer_norm", "gamma1", "beta1")
+transformer.add_layer_norm(model, "input_embed", "layer_norm", "gamma1", "beta1", std=0.774)
 transformer.add_attention(model, "layer_norm", tps.W_q, tps.W_k, tps.W_v, tps.W_o, tps.b_q, tps.b_k, tps.b_v, tps.b_o)
 transformer.add_residual_connection(model,"input_embed", "attention_output", "residual_1")
 transformer.add_layer_norm(model, "residual_1", "layer_norm_2", "gamma2", "beta2")
