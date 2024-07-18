fcd9ec1e166dd92dffdf8f8a4671439bf1bd8512
s-hallsworth <s.d.hallsworth@student.tudelft.nl>
2024-07-17 22:37:42 -0400

    chnage setup

---
+++ Pyomo_Toy/toy_problem_gurobi_seq_1.py
@@ -0,0 +1,178 @@
+import pyomo.environ as pyo
+from pyomo import dae
+import numpy as np
+import transformer
+import extract_from_pretrained
+import transformer as TNN
+import toy_problem_setup as tps
+import os
+from omlt import OmltBlock
+import convert_pyomo
+from gurobipy import Model, GRB, GurobiError
+from gurobi_ml import add_predictor_constr
+from GUROBI_ML_helper import get_inputs_gurobipy_FNN
+from print_stats import solve_gurobipy
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = '0' # turn off floating-point round-off
+
+
+"""
+Add transformer instance/constraints to toy problem setup and solve GUROBIPY Model
+
+"""
+
+# create transformer instance   
+model = tps.model  
+config_file = '.\\data\\toy_config_relu_seqlen_1.json' 
+transformer = TNN.Transformer(model, config_file)      
+        
+# Define tranformer layers
+std=0.774
+
+transformer.embed_input(model, "input_param","input_embed", "variables")
+transformer.add_layer_norm(model, "input_embed", "layer_norm", "gamma1", "beta1", std)
+transformer.add_attention(model, "layer_norm", tps.W_q, tps.W_k, tps.W_v, tps.W_o, tps.b_q, tps.b_k, tps.b_v, tps.b_o)
+transformer.add_residual_connection(model,"input_embed", "attention_output", "residual_1")
+transformer.add_layer_norm(model, "residual_1", "layer_norm_2", "gamma2", "beta2")
+nn, input_nn, output_nn = transformer.get_fnn(model, "layer_norm_2", "ffn_1", (10,2), tps.parameters)
+transformer.add_residual_connection(model,"residual_1", "ffn_1", "residual_2")  
+transformer.add_avg_pool(model, "residual_2", "avg_pool")
+nn2, input_nn2, output_nn2 = transformer.get_fnn(model, "avg_pool", "ffn_2", (1,2), tps.parameters)
+
+## Add constraint to input_var
+model.input_var_constraints = pyo.ConstraintList()
+
+for d in model.model_dims:
+    model.input_var_constraints.add(expr=model.input_var[model.time.last(),d] == model.ffn_2[d])
+
+# # Convert to gurobipy
+gurobi_model, map_var = convert_pyomo.to_gurobi(model)
+
+## Add FNN1 to gurobi model
+inputs_1, outputs_1 = get_inputs_gurobipy_FNN(input_nn, output_nn, map_var)
+pred_constr1 = add_predictor_constr(gurobi_model, nn, inputs_1, outputs_1)
+
+inputs_2, outputs_2 = get_inputs_gurobipy_FNN(input_nn2, output_nn2, map_var)
+pred_constr2 = add_predictor_constr(gurobi_model, nn2, inputs_2, outputs_2)
+gurobi_model.update()
+#pred_constr.print_stats()
+
+## Print Header
+print("------------------------------------------------------")
+print("                  SOLVE INFORMATION ")
+print("------------------------------------------------------")
+print()
+
+print(f"Problem: Toy Problem")
+print(f"Solver: Gurobi")
+print(f"Exp Approx: Dynamic Outer Bound")
+print(f"NN formulation: Gurobipy NN (using max constraint)")
+print("------------------------------------------------------")
+print()
+
+## Optimize
+gurobi_model.params.SolutionLimit = 1 ##
+gurobi_model.params.MIPFocus = 1 ## focus on finding feasible solution
+time_limit = 86400 # 24 hrs
+solve_gurobipy(gurobi_model, time_limit) ## Solve and print
+
+## Get optimal parameters
+if gurobi_model.status == GRB.OPTIMAL:
+    optimal_parameters = {}
+    for v in gurobi_model.getVars():
+        #print(f'var name: {v.varName}, var type {type(v)}')
+        if "[" in v.varName:
+            name = v.varname.split("[")[0]
+            if name in optimal_parameters.keys():
+                optimal_parameters[name] += [v.x]
+            else:
+                optimal_parameters[name] = [v.x]
+        else:    
+            optimal_parameters[v.varName] = v.x
+            
+##
+try:
+    print(
+    "objective value:", gurobi_model.getObjective().getValue()
+    )
+except:
+    gurobi_model.computeIIS()
+    gurobi_model.write("model.ilp")            
+            
+
+## Print Objective Value
+print(
+    "objective value:", gurobi_model.getObjective().getValue()
+)
+
+## Print X, U --> input var, control var 
+input_var_soltuion = np.array(optimal_parameters["input_var"])
+
+x = []
+u = []
+for i in range(len(input_var_soltuion)):
+    if i % 2 == 0:
+        x += [input_var_soltuion[i]]
+    else: 
+        u += [input_var_soltuion[i]]
+print("X: ", x )
+print("U: ", u )
+
+## Expected values:
+# x_input = [1.0, 1.10657895, 1.21388889, 1.32205882, 1.43125, 1.54166667, 1.65357143, 1.76730769, 1.88333333, 2.00227273, 2.125]
+# u_input = [0.25, 0.26315789, 0.27777778, 0.29411765, 0.3125, 0.33333333, 0.35714286, 0.38461538, 0.41666667, 0.45454545, 0.5]
+
+# # -------------------- SOLVE MODEL ----------------------------------- #
+
+def get_optimal_dict(result, model):
+    optimal_parameters = {}
+    if result.solver.status == 'ok' and result.solver.termination_condition == 'optimal':
+        for varname, var in model.component_map(pyo.Var).items():
+            # Check if the variable is indexed
+            if var.is_indexed():
+                optimal_parameters[varname] = {index: pyo.value(var[index]) for index in var.index_set()}
+            else:
+                optimal_parameters[varname] = pyo.value(var)
+        #print("Optimal Parameters:", optimal_parameters)
+    else:
+        print("No optimal solution obtained.")
+    
+    return optimal_parameters
+
+
+# from pyomo.core import *
+# from pyomo.opt import SolverFactory # run with python3.10
+# keepfiles = False  # True prints intermediate file names (.nl,.sol,...)
+
+# solver = SolverFactory('gurobi', solver_io='python')
+# #opts = {'halt_on_ampl_error': 'yes','tol': 1e-7, 'bound_relax_factor': 1.0}
+
+# #Create an IMPORT Suffix to store the iis information that willbe returned by gurobi_ampl
+# model.iis = Suffix(direction=Suffix.IMPORT)
+
+# ## Send the model to gurobi_ampl and collect the solution
+# #The solver plugin will scan the model for all active suffixes
+# # valid for importing, which it will store into the results object
+# results = solver.solve(model, keepfiles=keepfiles, tee=True)
+# print(results)
+
+
+# print("")
+# print("IIS Results")
+
+# for component, value in model.iis.items():
+#     print(component.name + " " + str(value))
+
+# model.compatibility.pprint()
+#---------------
+# import pyomo
+# #pyomo.contrib.mis.compute_infeasibility_explanation(model, solver=solver)
+# pyomo.contrib.iis.write_iis(model, "iss_file.ilp", solver='scip')
+
+# result = solver.solve(model)
+# # get optimal parameters & reformat  --> (1, input_feature, sequence_element)
+# optimal_parameters = get_optimal_dict(results, model)
+# print(optimal_parameters)
+# result = solver.solve(model) #, symbolic_solver_labels=True, tee=True, load_solutions=True, options=opts)
+
+# print(optimal_parameters)
--- Pyomo_Toy/toy_problem_setup.py
+++ Pyomo_Toy/toy_problem_setup.py
@@ -8,19 +8,21 @@ import data_gen
 Define toy problem parametrs and var then run from another script like toy_problem.py or transformer_test.py
 """
 
-NOT_WARM = False
+NOT_WARM = True
 ## read model weights
-#model_path = "..\\Transformer_Toy\\transformer_small_relu_2_seqlen_2.keras"
-model_path = "..\\Transformer_Toy\\transformer_small_relu_2_TOY.keras" 
+model_path = "..\\Transformer_Toy\\transformer_small_seqlen_1.keras"
+#model_path = "..\\Transformer_Toy\\transformer_small_relu_2_TOY.keras" 
 layer_names, parameters, TNN_model = extract_from_pretrained.get_learned_parameters(model_path)
 
 ## create model
 model = pyo.ConcreteModel(name="(TOY_TEST)")
 
 ## define problem sets, vars, params
-T = 11 #11
+T = 3 #11
+T_input = 0
+
 time = np.linspace(0, 1, num=T) # entire time t=0:1 including prediction times
-model.time_input = dae.ContinuousSet(initialize=time[:-1]) # t < prediction times
+model.time_input = dae.ContinuousSet(initialize=time[T_input]) # t < prediction times
 model.time = dae.ContinuousSet(initialize=time)
 set_variables = ['0','1'] ##--- NB: same order as trained input ---##
 model.variables = pyo.Set(initialize=set_variables)
@@ -34,47 +36,15 @@ if NOT_WARM:
     ## USE last 10 of 9000 data points
     # x_input = data_gen.x[0, -10:]
     # u_input = data_gen.u[0, -10:]
-    
-    x_input = data_gen.x[0, -3:]
-    u_input = data_gen.u[0, -3:]
+    #
+    x_input = data_gen.x[0, -T:]
+    u_input = data_gen.u[0, -T:]
     
     model.input_param = pyo.Var(model.time_input, model.variables, bounds=(LB_input, UB_input))
     model.input_var = pyo.Var(model.time, model.variables, bounds=(LB_input, UB_input)) #t = 0 to t=1
     
 else:
-    x_input = [1.0, 1.10657895, 1.21388889, 1.32205882, 1.43125, 1.54166667, 1.65357143, 1.76730769, 1.88333333, 2.00227273] #, 2.125]
-    u_input = [0.25, 0.26315789, 0.27777778, 0.29411765, 0.3125, 0.33333333, 0.35714286, 0.38461538, 0.41666667, 0.45454545] #, 0.5]
-    
-    # x_input = data_gen.x[0, -3:-2]
-    # u_input = data_gen.u[0, -3:-2]
-    
-    dict_inputs = {}
-    for t, (u_val, x_val) in zip(model.time_input, zip(u_input, x_input)):
-        dict_inputs[(t, '0')] = x_val
-        dict_inputs[(t, '1')] = u_val
-        
-    # model.input_param = pyo.Param(model.time_input, model.variables, initialize=dict_inputs)
-    model.input_param = pyo.Var(model.time_input, model.variables, bounds=(LB_input, UB_input)) # t=0 to t=prediction time
-    
-    model.input_param_fixed = pyo.Param(model.time_input, model.variables, initialize=dict_inputs) # t=0 to t=prediction time
-    model.x_param_fix_constraints = pyo.ConstraintList()  
-    for t in model.time_input:
-        for d in model.variables:
-            if t < 1 :
-                model.x_param_fix_constraints.add(expr=model.input_param[t,d] == model.input_param_fixed[t,d])
-
-    X =  [1.0, 1.10657895, 1.21388889, 1.32205882, 1.43125, 1.54166667, 1.65357143, 1.76730769, 1.88333333, 2.00227273, 1.4942082707791076]
-    U =  [0.25, 0.26315789, 0.27777778, 0.29411765, 0.3125, 0.33333333, 0.35714286, 0.38461538, 0.41666667, 0.45454545, 0.33872000390697854]
-
-    #X = data_gen.x[0, -3:]
-    # U = data_gen.u[0, -3:]
-    
-    dict_outputs = {}
-    for t, (u_out, x_out) in zip(model.time, zip(U, X)):
-        dict_outputs[(t, '0')] = x_out
-        dict_outputs[(t, '1')] = u_out
-    model.input_var = pyo.Var(model.time, model.variables, initialize=dict_outputs, bounds=(LB_input, UB_input)) #t = 0 to t=1
-
+    print("error")
 
 ## define transformer sets, vars, params
 dict_gamma1 = {(v): val for v,val in zip(model.variables, parameters['layer_normalization_1','gamma'])}
@@ -112,11 +82,7 @@ if NOT_WARM:
             model.x_init_constr_x = pyo.Constraint(expr=model.input_param[t,'0'] == x_input[0])
             model.x_init_constr_u = pyo.Constraint(expr=model.input_param[t,'1'] == u_input[0])
             
-        elif t < model.time.last():
-            # add constraints that x,u = x,u input values
-            model.input_constraints.add(expr=model.input_var[t,'0'] == model.input_param[t,'0'])
-            model.input_constraints.add(expr=model.input_var[t,'1'] == model.input_param[t,'1'])
-            
+        elif t <= model.time.last():
             # increasing var with time
             model.input_constraints.add(expr=model.input_var[t,'0'] >= model.input_var[model.time.at(t_index ),'0'])
             model.input_constraints.add(expr=model.input_var[t,'1'] >= model.input_var[model.time.at(t_index ),'1']) 
@@ -124,21 +90,7 @@ if NOT_WARM:
 else:  
     
     
-    for t_index, t in enumerate(model.time):
-        if t == model.time.first():
-            model.x_init_constr = pyo.Constraint(expr=model.input_var[t,'0'] == 1)
-            model.input_constraints.add(expr=model.input_var[t,'1'] == model.input_param[t,'1'])
-            
-        # create arrays for input x and u values
-        elif t < model.time.last():
-            # add constraints that x,u = x,u input values
-            model.input_constraints.add(expr=model.input_var[t,'0'] == model.input_param[t,'0'])
-            model.input_constraints.add(expr=model.input_var[t,'1'] == model.input_param[t,'1'])  
-            
-            # increasing var with time
-            model.input_constraints.add(expr=model.input_var[t,'0'] >= model.input_var[model.time.at(t_index ),'0'])
-            model.input_constraints.add(expr=model.input_var[t,'1'] >= model.input_var[model.time.at(t_index ),'1'])    
-    
+    print("error")
 # define integral constraints
 def _intX(m, t):
     return m.input_var[t,'0']
